1190f6dc2f0498e711566bc0352f4fe2f04b3bc2
陈栋明 <chendm0602@163.com>
2020-06-04 18:06:50 +0800

    初始环境

---
+++ admin/src/views/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>欢迎进入后台管理系统</div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
