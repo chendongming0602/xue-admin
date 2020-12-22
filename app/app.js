9dd9a31481eb600322c5b2231319f7464f8684d1
陈栋明 <chendm0602@163.com>
2020-12-22 09:24:01 +0800

    添加说明文件

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ const userrouter=require("./router/user.js");//用户信息路由
 const cors=require("cors");//解决跨域的中间件
 var server=express();
 server.listen(3000);
+console.log("node后端 启动成功")
 server.use(cors({//统一伪装跨域，之后不用再res.writeHead
 	origin:["http://localhost:8080"]
   }))
